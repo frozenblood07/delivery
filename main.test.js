cd1c3988962e7dadd05fd954a862af2c19de1596
Karan Tuteja <karan.tuteja26@gmail.com>
2018-07-02 22:17:12 +0530

    total allroutes functionality added. + tests

--- main.test.js
+++ main.test.js
@@ -4,6 +4,13 @@ beforeEach(() => {
   expect.assertions(1);
 });
 
+test('Executing `main.js` should respond with `No input provided`', () => {
+    
+    return exec('node main.js')
+    .then((result) => {
+        return expect(result.stdout.trim('\n')).toEqual('No input provided')
+    }) 
+ })
 
 test('Executing `main.js cost ABE` should respond with `4`', () => {  
     return exec('node main.js cost ABE')
@@ -45,5 +52,19 @@ test('Executing `main.js shortestPath EE` should respond with `6`', () => {
     return exec('node main.js shortestPath EE')
     .then((result) => {
         return expect(result.stdout.trim('\n')).toEqual('6')
+    }) 
+})
+
+test('Executing `main.js allRoutes ED4` should respond with `4`', () => {  
+    return exec('node main.js allRoutes ED4')
+    .then((result) => {
+        return expect(result.stdout.trim('\n')).toEqual('4')
+    }) 
+})
+
+test('Executing `main.js allRoutes EE0` should respond with `5`', () => {  
+    return exec('node main.js allRoutes EE0')
+    .then((result) => {
+        return expect(result.stdout.trim('\n')).toEqual('5')
     }) 
 })
